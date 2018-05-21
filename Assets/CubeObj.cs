1c1690200d8fb660f1585676668f46a4609b92f9
MaraBlack <36846988+MaraBlack@users.noreply.github.com>
2018-05-21 15:48:57 +0300

    Add files via upload

---
+++ Assets/CubeObj.cs
@@ -0,0 +1,128 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(BoxCollider))]
+public class CubeObj : MonoBehaviour {
+
+    public GameObject current; //non highlighted object
+    public GameObject highlighted; //highlighted object
+    public bool isPanelUp; // by default it is 0 since it is a field/class member.
+
+    private Vector3 dragPosition;
+
+    private Vector3 screenPoint;
+    private Vector3 offset;
+
+
+    void OnMouseEnter()
+    {
+        // if panel is NOT up then go ahead and switch sprites!
+        if (!isPanelUp)
+        {
+            current.SetActive(false);
+            highlighted.SetActive(true);
+
+           
+        }
+    }
+
+    void OnMouseExit()
+    {
+        // if panel is NOT up then go ahead and switch sprites!
+        if (!isPanelUp)
+        {
+            highlighted.SetActive(false);
+            current.SetActive(true);
+        }
+    }
+
+    // Use this for initialization
+    void Start () {
+
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        //transform.position = dragPosition;
+    }
+
+
+ 
+        private float _lockedYPosition;
+
+        void OnMouseDown()
+        {
+            //screenPoint = Camera.main.WorldToScreenPoint(gameObject.transform.position); // I removed this line to prevent centring 
+            _lockedYPosition = screenPoint.y;
+            offset = gameObject.transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z));
+        // Screen.showCursor = false;
+
+
+
+        }
+
+        void OnMouseDrag()
+        {
+            Vector3 curScreenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z);
+            Vector3 curPosition = Camera.main.ScreenToWorldPoint(curScreenPoint) + offset;
+            curPosition.x = _lockedYPosition;
+            //transform.position = curPosition;
+        }
+
+        void OnMouseUp()
+        {
+        // Screen.showCursor = true;
+
+    }
+
+    void getObjectWidth(GameObject myObj, GameObject myLabel)
+    {
+        Renderer renderer = myObj.GetComponent<Renderer>();
+        Bounds bounds = renderer.bounds;
+        float width = bounds.size.x;
+
+        Debug.Log(width);
+
+        var wallWidth = myLabel.GetComponentInChildren<Text>();
+        wallWidth.text = "x: " + width.ToString();
+    }
+
+    void getObjectHeight(GameObject myObj, GameObject myLabel)
+    {
+        Renderer renderer = myObj.GetComponent<Renderer>();
+        Bounds bounds = renderer.bounds;
+        float height = bounds.size.y;
+
+        Debug.Log(height);
+
+        var wallWidth = myLabel.GetComponentInChildren<Text>();
+        wallWidth.text = "y: " + height.ToString();
+    }
+
+    void get2DObjectDimensions(GameObject myObj, GameObject myLabel)
+    {
+        Renderer renderer = myObj.GetComponent<Renderer>();
+        Bounds bounds = renderer.bounds;
+        float width = bounds.size.x;
+        float height = bounds.size.y;
+
+        var wallWidth = myLabel.GetComponentInChildren<Text>();
+        wallWidth.text = "w:" + width.ToString() + "y" + height.ToString();
+    }
+
+    public void get3DObjectDimensions(GameObject myObj, GameObject myLabel)
+    {
+        Renderer renderer = myObj.GetComponent<Renderer>();
+        Bounds bounds = renderer.bounds;
+        float width = bounds.size.x;
+        float height = bounds.size.y;
+        float z = bounds.size.z;
+
+        var wallWidth = myLabel.GetComponentInChildren<Text>();
+        wallWidth.text = "W: " + width.ToString() + "\tY " + height.ToString() + "\tZ " + z.ToString();
+    }
+}
---
+++ Assets/DraggableObject.cs
@@ -0,0 +1,196 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+
+
+/*              y
+ *             |
+ *             |
+ *             |_________ x
+ *            /
+ *           /
+ *          / z
+ * 
+ */
+
+public class DraggableObject : MonoBehaviour
+{
+    //Plane movePlane;
+    //float fixedDistance = 2f;
+    //float hitDist, t;
+    //Ray camRay;
+    //Color origColor;
+    //Vector3 startPos, point, corPoint;
+    //private bool isSelected = false;
+    //[SerializeField]
+    //Color selectedColor = Color.yellow;
+    //[SerializeField]
+    //Color colidingColor = Color.red;
+    //private int rotationDegrees;
+    //Vector3 originalPosition;
+    //int nrColiders;
+
+    //CubeObj co = new CubeObj();
+    //void OnMouseDown()
+    //{
+    //    originalPosition = transform.position;
+    //    Debug.Log("MouseDown");
+    //    Renderer rend = GetComponent<Renderer>();
+    //    if (rend == null)
+    //    {
+    //        foreach (Renderer r in GetComponentsInChildren<Renderer>())
+    //        {
+    //            r.material.color = selectedColor;
+    //        }
+    //    }
+    //    else
+    //    {
+    //        rend.material.color = selectedColor;
+    //    }
+    //    startPos = transform.position; // save position in case draged to invalid place
+    //    movePlane = new Plane(-Camera.main.transform.forward, transform.position); // find a parallel plane to the camera based on obj start pos;
+    //    if(!isSelected)
+    //        GameObject.Find("Main Camera").GetComponent<SceneManager>().SetSelectObject(gameObject);
+    //    isSelected = true;
+    //    //GetComponent<Rigidbody>().isKinematic = false;
+
+    //    var wLabel = GameObject.Find("objDimension");
+
+    //   // co.get3DObjectDimensions(gameObject, wLabel);
+
+
+    //}
+
+    ////rigidbody.MovePosition instead of transform.position.
+    //void OnMouseDrag()
+    //{
+    //    camRay = Camera.main.ScreenPointToRay(Input.mousePosition); // shoot a ray at the obj from mouse screen point
+
+    //    if (movePlane.Raycast(camRay, out hitDist))
+    //    { // finde the collision on movePlane
+    //        point = camRay.GetPoint(hitDist); // define the point on movePlane
+    //        t = -(fixedDistance - camRay.origin.y) / (camRay.origin.y - point.y); // the x,y or z plane you want to be fixed to
+    //        corPoint.x = camRay.origin.x + (point.x - camRay.origin.x) * t; // calculate the new point t futher along the ray
+    //        corPoint.z = camRay.origin.z + (point.z - camRay.origin.z) * t;
+    //        //transform.position = corPoint;
+    //        GetComponent<Rigidbody>().MovePosition(corPoint);           
+
+    //    }
+    //}
+
+    //public void deselect()
+    //{
+    //    Renderer rend = GetComponent<Renderer>();
+    //    if (rend == null)
+    //    {
+    //        foreach (Renderer r in GetComponentsInChildren<Renderer>())
+    //        {
+    //            r.material.color = origColor;
+    //        }
+    //    }
+    //    else
+    //    {
+    //        rend.material.color = origColor;
+    //    }
+        
+    //    isSelected = false;
+    //}
+
+    //private void OnMouseUp()
+    //{
+    //    if (nrColiders > 0)
+    //        GetComponent<Rigidbody>().MovePosition(originalPosition);
+    //    //GetComponent<Rigidbody>().MovePosition(corPoint);
+    //    //GetComponent<Rigidbody>().isKinematic = true;
+    //}
+
+    //private void OnTriggerEnter(Collider other)
+    //{
+    //    //transform.position = new Vector3(0, 0, 0);
+    //    Debug.Log("b;a");
+    //}
+
+    //private void OnCollisionEnter(Collision collision)
+    //{
+    //    Renderer rend = GetComponent<Renderer>(); 
+    //    if (rend == null)
+    //    {
+    //        foreach (Renderer r in GetComponentsInChildren<Renderer>())
+    //        {
+    //            r.material.color = colidingColor;
+    //        }
+    //    }
+    //    else
+    //    {
+    //        rend.material.color = colidingColor;
+    //    }
+    //    nrColiders++;
+
+    //}
+
+    //private void OnCollisionExit(Collision collision)
+    //{
+    //    Renderer rend = GetComponent<Renderer>();
+    //    //if (rend == null)
+    //    //    rend = GetComponentInChildren<Renderer>();
+    //    nrColiders--;
+    //    if (nrColiders > 0)
+    //        return;
+    //    if (rend == null)
+    //    {
+    //        foreach (Renderer r in GetComponentsInChildren<Renderer>())
+    //        {
+    //            if (isSelected)
+    //                r.material.color = selectedColor;
+    //            else
+    //            {
+    //                r.material.color = origColor;
+    //            }
+    //        }
+    //    }
+    //    else
+    //    {
+    //        if (isSelected)
+    //            rend.material.color = selectedColor;
+    //        else
+    //        {
+    //            rend.material.color = origColor;
+    //        }
+    //    }
+       
+        
+    //}
+
+    //public void Rotate(float degrees)
+    //{
+    //    rotationDegrees = (int)degrees;
+    //    transform.Rotate(Vector3.up,   (degrees*45) - transform.rotation.eulerAngles.y, Space.World);
+
+
+    //}
+
+    //public void Delete(GameObject go)
+    //{
+    //    //if (isSelected)
+    //        //Destroy(go);
+    //}
+
+    //private void Start()
+    //{
+    //    corPoint = transform.position;
+    //    Renderer rend = GetComponent<Renderer>();
+    //    if(rend == null)
+    //    {
+    //        rend = GetComponentInChildren<Renderer>();
+    //    }
+    //    origColor = rend.material.color;
+    //    GetComponent<Rigidbody>().freezeRotation = true;
+    //    nrColiders = 0;
+    //}
+
+    //public int getRotationDegrees()
+    //{
+    //    return rotationDegrees;
+    //}
+}
